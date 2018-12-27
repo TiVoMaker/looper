a16d79b1db9e49b1a28b32e08eb8a57cb83b30a7
Jim Barton <jmb@inyankara.org>
2018-12-26 16:18:41 -0800

    Treating it as a polyfill.

--- index.js
+++ index.js
@@ -2,53 +2,57 @@
  * Copyright (C) 2018 James M. Barton
  */
 
-Object.defineProperty(Promise, 'looper', {
-    value: function (worker, checker, initialValue) {
-        return new Promise((ok, fail) => {
+if (!Promise.looper) {
+    Object.defineProperty(Promise, 'looper', {
+        value: function (worker, checker, initialValue) {
+            return new Promise((ok, fail) => {
 
-            function runOnce(nextValue) {
-                if (nextValue instanceof Promise) {
-                    nextValue.then(newValue => {
-                        runOnce(newValue);
-                    }).catch(err => {
-                        fail(err);
-                    });
-                } else {
-                    if (checker(nextValue)) {
-                        setTimeout(() => runOnce(worker(nextValue)), 0);
+                function runOnce(nextValue) {
+                    if (nextValue instanceof Promise) {
+                        nextValue.then(newValue => {
+                            runOnce(newValue);
+                        }).catch(err => {
+                            fail(err);
+                        });
                     } else {
-                        ok(nextValue);
+                        if (checker(nextValue)) {
+                            setTimeout(() => runOnce(worker(nextValue)), 0);
+                        } else {
+                            ok(nextValue);
+                        }
                     }
                 }
-            }
 
-            if (initialValue !== undefined) {
-                if (initialValue instanceof Promise) {
-                    initialValue.then(newValue => {
-                        if (!checker(newValue)) {
-                            ok(newValue);
-                            return;
-                        }
-                        runOnce(newValue);
-                    }).catch(err => {
-                        fail(err);
-                    });
-                    return;
+                if (initialValue !== undefined) {
+                    if (initialValue instanceof Promise) {
+                        initialValue.then(newValue => {
+                            if (!checker(newValue)) {
+                                ok(newValue);
+                                return;
+                            }
+                            runOnce(newValue);
+                        }).catch(err => {
+                            fail(err);
+                        });
+                        return;
+                    }
+                    if (!checker(initialValue)) {
+                        ok(initialValue);
+                        return;
+                    }
                 }
-                if (!checker(initialValue)) {
-                    ok(initialValue);
-                    return;
-                }
-            }
-            runOnce(worker(initialValue));
-        });
-    }
-});
+                runOnce(worker(initialValue));
+            });
+        }
+    });
+}
 
-Object.defineProperty(Promise, 'waiter', {
-    value: function(ms) {
-        return new Promise(ok => {
-            setTimeout(() => ok(), ms);
-        });
-    }
-});
+if (!Promise.waiter) {
+    Object.defineProperty(Promise, 'waiter', {
+        value: function(ms) {
+            return new Promise(ok => {
+                setTimeout(() => ok(), ms);
+            });
+        }
+    });
+}
